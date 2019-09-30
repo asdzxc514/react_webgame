2ecf7724885ef89317ec5560c5012ba83c4ea083
김혜지니 <gpwls514@gmail.com>
2019-09-30 10:43:54 +0900

    Updates

---
+++ precache-manifest.377316e777cd3fb7c9c6c7d12892fdfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d1e6442d8498e3c1845629475886cd",
+    "url": "/react_webgame/EGOING-APP/index.html"
+  },
+  {
+    "revision": "c685fe78d4e62d556b32",
+    "url": "/react_webgame/EGOING-APP/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9539ce627cc0f6409a4b",
+    "url": "/react_webgame/EGOING-APP/static/js/2.0c9ce092.chunk.js"
+  },
+  {
+    "revision": "c685fe78d4e62d556b32",
+    "url": "/react_webgame/EGOING-APP/static/js/main.81bb13c5.chunk.js"
+  },
+  {
+    "revision": "1f9e9a45a35ed04d73f0",
+    "url": "/react_webgame/EGOING-APP/static/js/runtime-main.1ac9cd05.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react_webgame/EGOING-APP/static/media/logo.25bf045c.svg"
+  }
+]);
