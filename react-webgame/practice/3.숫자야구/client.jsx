f8ed8ca53d222e9c73422108cb4e25cade232c81
김혜지니 <gpwls514@gmail.com>
2019-09-30 10:25:54 +0900

    Updates

--- react-webgame/practice/3.숫자야구/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';  // { hot } 은 구조분해 문법이다.
-
-import NumberBaseball from './NumberBaseball_hooks';
-
-const Hot = hot(NumberBaseball);
-
-ReactDom.render(<Hot />, document.querySelector('#root')); 
--- react-webgame/practice/6.로또/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-import Lotto from './lotto_hooks';
-
-const Hot = hot(Lotto);
-
-ReactDom.render(<Hot />, document.querySelector('#root'));
